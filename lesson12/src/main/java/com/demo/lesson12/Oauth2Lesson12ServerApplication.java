064e456b2ac54e6af159f175f9d586c65af4b1bc
forever1986 <forever1986@163.com>
2025-01-20 09:06:10 +0800

    高级特性：OIDC1.0

---
+++ lesson12/src/main/java/com/demo/lesson12/Oauth2Lesson12ServerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.lesson12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2Lesson12ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2Lesson12ServerApplication.class, args);
+    }
+
+}
---
+++ lesson12/src/main/java/com/demo/lesson12/config/SecurityConfig.java
@@ -0,0 +1,85 @@
+package com.demo.lesson12.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+import java.util.UUID;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    // 自定义授权服务器的Filter链
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+                // oidc配置
+                .oidc(withDefaults());
+        // 同时作为资源服务器，使用/userinfo接口
+        http.oauth2ResourceServer((resourceServer) -> resourceServer.jwt(withDefaults()));
+        // 异常处理
+        http.exceptionHandling((exceptions) -> exceptions.authenticationEntryPoint(
+                new LoginUrlAuthenticationEntryPoint("/login")));
+        return http.build();
+    }
+
+    // 自定义Spring Security的链路。如果自定义授权服务器的Filter链，则原先自动化配置将会失效，因此也要配置Spring Security
+    @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorize) -> authorize
+                .anyRequest().authenticated()).formLogin(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public RegisteredClientRepository registeredClientRepository() {
+        RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                // 客户端id
+                .clientId("oidc-client")
+                // 客户端密码
+                .clientSecret("{noop}secret")
+                // 客户端认证方式
+                .clientAuthenticationMethods(methods ->{
+                    methods.add(ClientAuthenticationMethod.CLIENT_SECRET_BASIC);
+                })
+                // 配置授权码模式
+                .authorizationGrantTypes(grantTypes -> {
+                    grantTypes.add(AuthorizationGrantType.AUTHORIZATION_CODE);
+                })
+                // 需要授权确认
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                // 回调地址
+                .redirectUri("http://localhost:8080/login/oauth2/code/oidc-client")
+                .postLogoutRedirectUri("http://localhost:8080/")
+                // 授权范围
+                .scopes(scopes->{
+                    scopes.add(OidcScopes.OPENID);
+                    scopes.add(OidcScopes.PROFILE);
+                    scopes.add(OidcScopes.EMAIL);
+                })
+                .build();
+
+        return new InMemoryRegisteredClientRepository(registeredClient);
+    }
+
+}
