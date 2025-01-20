5e4aa776829334f08f7c5beb564c3e355e5a9f21
forever1986 <forever1986@163.com>
2025-01-20 09:05:18 +0800

    高级特性：设备授权码模式

---
+++ lesson11/src/main/java/com/demo/lesson11/Oauth2Lesson11ServerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.lesson11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2Lesson11ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2Lesson11ServerApplication.class, args);
+    }
+
+}
