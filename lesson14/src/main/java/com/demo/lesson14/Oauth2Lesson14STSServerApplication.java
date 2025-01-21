e26f1cdb53e98c5995353278b9be9e7f00562043
forever1986 <forever1986@163.com>
2025-01-21 10:53:45 +0800

    高级特性：令牌交换

---
+++ lesson14/src/main/java/com/demo/lesson14/Oauth2Lesson14STSServerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.lesson14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2Lesson14STSServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2Lesson14STSServerApplication.class, args);
+    }
+
+}
